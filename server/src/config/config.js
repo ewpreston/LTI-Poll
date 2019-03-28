4641ecf8f5c550939de15dda16c10d56c100fbd5
Eric Preston <eric.preston@blackboard.com>
2019-03-28 16:08:28 -0500

    use env vars for config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -28,7 +28,18 @@ if (process.env.REDIS_PORT_6379_TCP_ADDR) {
 if (process.env.REDIS_PORT_6379_TCP_PORT) {
   configResult["redis_port"] = process.env.REDIS_PORT_6379_TCP_PORT;
 }
-
+if (process.env.REDIS_URL) {
+  configResult["redis_url"] = process.env.REDIS_URL;
+}
+if (process.env.LTI_POLL_PROVIDER_DOMAIN) {
+  configResult["provider_domain"] = process.env.LTI_POLL_PROVIDER_DOMAIN;
+}
+if (process.env.LTI_POLL_PROVIDER_PORT) {
+  configResult["provider_port"] = process.env.LTI_POLL_PROVIDER_PORT;
+}
+if (process.env.LTI_POLL_USE_SSL) {
+  configResult["use_ssl"] = process.env.LTI_POOL_USE_SSL;
+}
 export default _.defaultsDeep(configResult, configJson);
 
 console.log(JSON.stringify(configResult, null, 2));
