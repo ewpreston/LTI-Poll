6b5146b016a75df62f5f13e4411afa502b1c4e71
Eric Preston <eric.preston@blackboard.com>
2019-03-28 16:06:13 -0500

    Revert "removed launchEndpoint"
    This reverts commit 5df627436128741f690906fb692942292330689e.

--- server/src/common/restTypes.js
+++ server/src/common/restTypes.js
@@ -1,3 +1,18 @@
+export class TCProfileResponse {
+  constructor(profile, returnUrl) {
+    this.toolConsumerProfile = profile;
+    this.launch_presentation_return_url = returnUrl;
+  }
+}
+
+export class RegistrationData {
+  constructor() {
+    this.TCProfileResponse = new TCProfileResponse();
+    this.status = "";
+    this.failReason = "";
+  }
+}
+
 export class ContentItem {
   constructor() {
     this.data = new Map([]);
