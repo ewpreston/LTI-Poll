df84ea0cf1026d1122bc499e0b51784f062fb027
Mike Brehm <mike.brehm@blackboard.com>
2019-04-04 16:22:57 -0400

    Revert app.js

--- public/src/app.js
+++ public/src/app.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Dashboard from "./components/dashboard";
-import PollSetup from "./components/pollSetup";
 
-ReactDOM.render(<PollSetup />, document.getElementById("root"));
+ReactDOM.render(<Dashboard />, document.getElementById("root"));
